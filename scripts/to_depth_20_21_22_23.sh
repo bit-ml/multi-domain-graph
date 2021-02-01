1adac5a74cc6558b4d9d4ad12be7dec87996547f
Emanuela Haller <haller.emanuela@gmail.com>
2021-02-01 13:35:13 +0200

    change configs

---
+++ scripts/to_depth_20_21_22_23.sh
@@ -0,0 +1,7 @@
+nohup python main.py ./configs/configs_train_01.02/train_to_depth_config_20.ini > ./logs/logs_train_01.02/to_depth_config_20.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_depth_config_21.ini > ./logs/logs_train_01.02/to_depth_config_21.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_depth_config_22.ini > ./logs/logs_train_01.02/to_depth_config_22.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_depth_config_23.ini > ./logs/logs_train_01.02/to_depth_config_23.out
---
+++ scripts/to_saliency_20_21_22_23.sh
@@ -0,0 +1,7 @@
+nohup python main.py ./configs/configs_train_01.02/train_to_saliency_config_20.ini > ./logs/logs_train_01.02/to_saliency_config_20.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_saliency_config_21.ini > ./logs/logs_train_01.02/to_saliency_config_21.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_saliency_config_22.ini > ./logs/logs_train_01.02/to_saliency_config_22.out
+wait
+nohup python main.py ./configs/configs_train_01.02/train_to_saliency_config_23.ini > ./logs/logs_train_01.02/to_saliency_config_23.out
