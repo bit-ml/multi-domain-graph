3ab76c142b1f2f87906730894020d1b6fc43e790
Emanuela Haller <haller.emanuela@gmail.com>
2021-03-11 10:11:59 +0200

    merge csvs

---
+++ analysis/merge_results.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import numpy as np
+import cv2
+
+path = '/root/code/multi-domain-graph/csv_results'
+
+files = os.listdir(path)
+files.sort()
+idx = 0
+final_path = 'final.csv'
+final_f = open(final_path, 'w')
+for file_ in files:
+    f = open(os.path.join(path, file_), 'r')
+    lis = [line for line in f]
+
+    if idx == 0:
+        final_f.write('%s' % lis[0])
+        idx = idx + 1
+    if len(lis) > 1:
+        final_f.write('%s' % lis[1])
+    f.close()
+
+final_f.close()
